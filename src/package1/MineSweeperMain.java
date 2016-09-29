3f200021ab4d11d414bf92e86fd0ce29f9e3b10f
Kate McGowan <katejomcgowan@gmail.com>
2016-09-29 19:26:47 -0400

    Added Main class to be filled in later...

---
+++ src/package1/MineSweeperMain.java
@@ -0,0 +1,15 @@
+package package1;
+
+/**
+ * 
+ * @author Kate McGowan, Adam Stewart, Sierra Ellison
+ * @version Beta 1.01
+ *
+ */
+
+public class MineSweeperMain {
+
+	public static void main(String args[]) {
+
+	}
+}
