615c3e035d7bc65880087c285d25c102cfde2076
Sierracy <ellisosi@mail.gvsu.edu>
2016-10-06 19:37:18 -0400

    Started Cell Unit Cases

---
+++ src/package1/CellTest.java
@@ -0,0 +1,17 @@
+package package1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CellTest {
+	
+	Cell testCell = new Cell();
+	
+
+	@Test
+	public void isMineTest() {
+		fail("Not yet implemented");
+	}
+
+}
