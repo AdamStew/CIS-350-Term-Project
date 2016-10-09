6937d1dbcee8fdde4f6d7afd608bdee857a8db50
Kate McGowan <katejomcgowan@gmail.com>
2016-10-09 15:18:58 -0400

    Created main constructor.

--- src/package1/MineSweeperMain.java
+++ src/package1/MineSweeperMain.java
@@ -1,5 +1,7 @@
 package package1;
 
+import javax.swing.JFrame;
+
 /**
  * 
  * @author Kate McGowan, Adam Stewart, Sierra Ellison
@@ -9,7 +11,13 @@ package package1;
 
 public class MineSweeperMain {
 
-	public static void main(String args[]) {
+  public static void main(String args[]) {
+    JFrame frame = new JFrame("Mine Sweeper");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	}
+    MineSweeperGUI GUI = new MineSweeperGUI();
+    frame.add(GUI);
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
