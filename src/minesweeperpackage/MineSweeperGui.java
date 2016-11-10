908b7bd961d91868376088cccc81d31fa560b283
Adam Stewart <stewarad@mail.gvsu.edu>
2016-11-10 17:25:51 -0500

    Minor bug fix.

--- src/minesweeperpackage/MineSweeperGui.java
+++ src/minesweeperpackage/MineSweeperGui.java
@@ -426,6 +426,7 @@ public class MineSweeperGui extends JFrame implements ActionListener, MouseListe
               }
               game.getCell(randRow, randCol).setMine(true);
               board[row][col].setIcon(null);
+              board[randRow][randCol].setIcon(mine);
             }
             firstMove = false;
             timer.schedule(new Updateclock(), 0, 1000);
